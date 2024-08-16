3c430e5b11901f106f2b55f1d113a56075b955ff
msahrana <msahrana@gmail.com>
2024-08-16 11:58:24 +0600

    server site is create successfully

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// [db code]
+
+app.get("/", (req, res) => {
+  res.send("SIYANA CARVILA server is running");
+});
+
+app.listen(port, () => {
+  console.log(`SIYANA CARVILA server is running on port: ${port}`);
+});
