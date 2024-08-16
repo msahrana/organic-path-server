8e2a00c1f763032663a8f23612f0917d62427bd6
msahrana <msahrana@gmail.com>
2024-08-16 12:27:06 +0600

    mongoDB setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const {MongoClient, ServerApiVersion} = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,7 +13,32 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// [db code]
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crgl3kb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // const dataCollection = client.db("organicDB").collection("allData");
+
+    // app.get("/all-data", async (req, res) => {
+    //   const result = await dataCollection.find().toArray();
+    //   res.send(result);
+    // });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Organic Path server is running");
