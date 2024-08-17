84e72ffaec124e2ad4677895bdea7bec097c00e5
msahrana <msahrana@gmail.com>
2024-08-17 12:35:14 +0600

    corsOptions section is updated

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://organic-path.web.app",
+    "https://organic-path.firebaseapp.com",
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 };
